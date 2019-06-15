e365cd6ed0597d05ff74ac0f0513397e5c34f214
Shahul Hameed <beingshahul@gmail.com>
2019-06-15 23:18:22 +0530

    Added a simple test case for verifying the homepage loads correctly.

--- journal/tests.py
+++ journal/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTests(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
