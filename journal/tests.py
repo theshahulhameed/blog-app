da008d21d5383674939d0f4585ddb9cdac17473d
Shahul Hameed <beingshahul@gmail.com>
2019-06-16 11:29:14 +0530

    Added another test case for verifying the template used.

--- journal/tests.py
+++ journal/tests.py
@@ -4,4 +4,9 @@ class SimpleTests(TestCase):
     def test_homepage_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+    
+    def test_homepage_uses_correct_template(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed('home.html')
 
