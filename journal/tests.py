ffc75150d7923370e0073701caee9a5ecd1e47b0
Shahul Hameed <beingshahul@gmail.com>
2019-06-16 11:34:52 +0530

    Added comments and a test for homepage.

--- journal/tests.py
+++ journal/tests.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 class SimpleTests(TestCase):
     def test_homepage_status_code(self):
+        ''' Tests whether the homepage returns works. '''
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
     
+    def test_homepage_status_by_url_name(self):
+        """ Tests whether the homepage can be accessed by the URL name 'home' """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
     def test_homepage_uses_correct_template(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
